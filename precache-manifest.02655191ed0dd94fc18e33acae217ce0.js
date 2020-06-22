cd03b40241dd69a9e3bcf3518ba53e4f959b3af3
Taral Pawar <taralpawar@gmail.com>
2020-06-22 12:34:01 +0530

    Updates

---
+++ precache-manifest.02655191ed0dd94fc18e33acae217ce0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca17ee6e57b52d605a0fa3fd1c2b9ee7",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "a3c5660e7da128c514de",
+    "url": "/react-weather-app/static/css/main.97c20ff0.chunk.css"
+  },
+  {
+    "revision": "c47199e6177ac8a228d7",
+    "url": "/react-weather-app/static/js/2.65616bf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.65616bf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c5660e7da128c514de",
+    "url": "/react-weather-app/static/js/main.fc627d55.chunk.js"
+  },
+  {
+    "revision": "5bbecaf8c6cbdf06df86",
+    "url": "/react-weather-app/static/js/runtime-main.e71492be.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/react-weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/react-weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
